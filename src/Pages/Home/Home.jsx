ea0e9fa7cd414281a9f60cdf052f7695d082d959
RamazanRahimli <162458672+RamazanRahimli@users.noreply.github.com>
2024-06-28 14:43:10 +0400

    Add files via upload

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Header from '../../Components/Header/Header'
 import Hero from '../../Components/Hero/Hero'
 import MenuSection from '../../Components/MenuSection/MenuSection'
+import ProductSection from '../../Components/ProductSection/ProductSection'
+import AboutSection from '../../Components/AboutSection/AboutSection'
+import ReviewSection from '../../Components/ReviewSection/ReviewSection'
+import ContactSection from '../../Components/ContactSection/ContactSection'
+import Footer from '../../Components/Footer/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +14,11 @@ const Home = () => {
       <Header/>
       <Hero/>
       <MenuSection/>
+      <ProductSection/>
+      <AboutSection/>
+      <ReviewSection/>
+      <ContactSection/>
+      <Footer/>
     </div>
   )
 }
